f94e41e80a97320b24c3cedc6e44510b453b5ebf
Alexander Yakimov <yakimov.alex2354@gmail.com>
2019-01-07 18:28:31 +0300

    Add notification of invalid rss

--- src/main/java/org/telegram/bot/command/SubscribeCommand.java
+++ src/main/java/org/telegram/bot/command/SubscribeCommand.java
@@ -1,12 +1,15 @@
 package org.telegram.bot.command;
 
+import com.rometools.rome.io.SyndFeedInput;
+import com.rometools.rome.io.XmlReader;
 import org.telegram.bot.hibernate.dao.SubscriptionDao;
 import org.telegram.bot.hibernate.model.Subscription;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Chat;
 import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.bots.AbsSender;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.sql.Timestamp;
 import java.util.logging.Level;
@@ -19,14 +22,24 @@ public class SubscribeCommand implements BotCommand {
     @Override
     public void execute(AbsSender sender, Chat chat, User user, String text) {
         try {
+            URL url = new URL(text);
+            new SyndFeedInput().build(new XmlReader(url));
             SubscriptionDao subscriptionDao = new SubscriptionDao();
             Subscription subscription = new Subscription(
                     chat.getId(),
                     new URL(text),
                     new Timestamp(System.currentTimeMillis()));
             subscriptionDao.add(subscription);
-        } catch (MalformedURLException ex) {
-            LOG.log(Level.SEVERE, "Failed to convert string to url", ex);
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "Invalid rss link", ex);
+            try {
+                SendMessage sendMessage = new SendMessage();
+                sendMessage.setChatId(chat.getId());
+                sendMessage.setText("Sorry! It isn't rss link");
+                sender.execute(sendMessage);
+            } catch (TelegramApiException e) {
+                LOG.log(Level.SEVERE, "Failed to send message");
+            }
         }
     }
 }
